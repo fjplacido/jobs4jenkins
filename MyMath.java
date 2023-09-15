a782d9b7a17be17270f367aa8ad47cf2b0f90f7a
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2023-09-15 19:42:53 -0300

    Test integration #1

--- MyMath.java
+++ MyMath.java
@@ -2,7 +2,7 @@ package jsolutions;
 
 public class MyMath {
 
-	// method that adds 2 numbers
+	// method that adds 2 numbers - testing
 	public Double add(double a, double b) {
 		return a + b;
 	}
